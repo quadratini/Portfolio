02394f5acef4e285e3724baaea2be6a14c4e0ec3
Ronny Ritprasert <ronnyrit@gmail.com>
2020-08-29 19:09:15 -0700

    added landing page, background, etc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// This is landing page
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container-fluid">
+      <div className="row" style={{height: "90vh"}}>
+      <div className="col-12 bg-primary">
+
+      </div>
+      </div>
+   </div>
   );
 }
 
